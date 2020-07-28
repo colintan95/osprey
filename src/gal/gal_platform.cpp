6e600bb87e74e122265f44b99d30116e60e6966b
Colin Tan <colintan95@gmail.com>
2020-07-28 06:59:26 -0700

    Added ChoosePhysicalDevice() for GALPlatform.

--- src/gal/gal_platform.cpp
+++ src/gal/gal_platform.cpp
@@ -4,6 +4,9 @@
 #include <optional>
 #include <vector>
 
+#include "gal/gal_exception.h"
+#include "window/window.h"
+
 namespace gal {
 
 namespace {
@@ -76,10 +79,122 @@ GALPlatform::GALPlatform(window::Window* window) {
   }
 
   vk_surface_ = window->CreateVkSurface(vk_instance_);
+
+  std::optional<PhysicalDeviceInfo> physical_device_info = ChoosePhysicalDevice();
+  if (!physical_device_info.has_value()) {
+    throw Exception("Could not find a suitable physical device.");
+  }
 }
 
 GALPlatform::~GALPlatform() {
   
+}
+
+std::optional<GALPlatform::PhysicalDeviceInfo> GALPlatform::ChoosePhysicalDevice() {
+  uint32_t physical_devices_count = 0;
+  vkEnumeratePhysicalDevices(vk_instance_, &physical_devices_count, nullptr);
+
+  std::vector<VkPhysicalDevice> physical_devices(physical_devices_count);
+  vkEnumeratePhysicalDevices(vk_instance_, &physical_devices_count, physical_devices.data());
+
+  bool found_physical_device = false;
+  PhysicalDeviceInfo result;
+
+  for (const VkPhysicalDevice& device : physical_devices) {
+
+    // Discrete GPU
+
+    VkPhysicalDeviceProperties device_props;
+    vkGetPhysicalDeviceProperties(device, &device_props);
+
+    if (device_props.deviceType != VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+      continue;
+    }
+
+    uint32_t queue_families_count = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_families_count, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queue_families(queue_families_count);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_families_count, queue_families.data());
+
+    // Graphics queue
+
+    bool found_graphics_queue = false;
+
+    int index = 0;
+    for (const VkQueueFamilyProperties& queue_family : queue_families) {
+      if (queue_family.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+        result.graphics_queue_family_index = index;
+        found_graphics_queue = true;
+        break;
+      }
+      ++index;
+    }
+    if (!found_graphics_queue) {
+      continue;
+    }
+
+    // Present queue
+
+    bool found_present_queue = false;
+
+    index = 0;
+    for (const VkQueueFamilyProperties& queue_family : queue_families) {
+      VkBool32 supports_present = false;
+      vkGetPhysicalDeviceSurfaceSupportKHR(device, index, vk_surface_, 
+                                            &supports_present);
+      if (supports_present) {
+        result.present_queue_family_index = index;
+        found_present_queue = true;
+        break;
+      }
+      ++index;
+    }
+    if (!found_present_queue) {
+      continue;
+    }
+
+    // Extensions
+
+    uint32_t extensions_count = 0;
+    vkEnumerateDeviceExtensionProperties(device, nullptr, &extensions_count, nullptr);
+
+    std::vector<VkExtensionProperties> extensions(extensions_count);
+    vkEnumerateDeviceExtensionProperties(device, nullptr, &extensions_count, extensions.data());
+
+    // TODO(colintan): Enumerate through a list of requested extensions and see if the physical
+    // device supports each.
+    if (std::find_if(extensions.begin(), extensions.end(), 
+            [](const VkExtensionProperties& props) { 
+              return strncmp(props.extensionName, VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+                             sizeof(VK_KHR_SWAPCHAIN_EXTENSION_NAME)) == 0;
+            }) == extensions.end()) {
+      continue;
+    }
+
+    // Surface formats and present modes - whether they are nonempty
+
+    uint32_t surface_formats_count = 0;
+    vkGetPhysicalDeviceSurfaceFormatsKHR(device, vk_surface_, &surface_formats_count, nullptr);
+    if (surface_formats_count == 0) {
+      continue;
+    }
+
+    uint32_t present_modes_count = 0;
+    vkGetPhysicalDeviceSurfacePresentModesKHR(device, vk_surface_, &present_modes_count, nullptr);
+    if (present_modes_count == 0) {
+      continue;
+    }
+
+    found_physical_device = true;
+    vk_physical_device_ = device;
+  }
+  
+  if (!found_physical_device) {
+    return std::nullopt;
+  }
+
+  return result;
 }
 
 } // namespace gal
--- src/gal/gal_platform.h
+++ src/gal/gal_platform.h
@@ -3,6 +3,7 @@
 
 #include <vulkan/vulkan.h>
 
+#include <optional>
 #include "gal/gal_exception.h"
 #include "window/window.h"
 
@@ -14,11 +15,21 @@ public:
   ~GALPlatform();
 
 private:
+  struct PhysicalDeviceInfo {
+    uint32_t graphics_queue_family_index;
+    uint32_t present_queue_family_index;
+  };
+
+  std::optional<PhysicalDeviceInfo> ChoosePhysicalDevice();
+
+private:
   window::Window* window_;
 
   VkInstance vk_instance_;
   VkDebugUtilsMessengerEXT vk_debug_messenger_;
   VkSurfaceKHR vk_surface_;
+
+  VkPhysicalDevice vk_physical_device_;
 };
 
 } // namespace gal

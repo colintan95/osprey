5b458dbe842f0d5b5c3e87e8756b1235ca585f22
Colin Tan <colintan95@gmail.com>
2020-07-28 18:12:00 -0700

    Added scaffolding for GALPipeline.

--- src/gal/gal_pipeline.cpp
+++ src/gal/gal_pipeline.cpp
@@ -1,6 +1,50 @@
 #include "gal/gal_pipeline.h"
 
+#include <memory>
+#include "gal/gal_exception.h"
+
 namespace gal {
 
+GALPipeline::GALPipeline(GALPipeline::Builder& builder) {
+
+}
+
+GALPipeline::Builder& GALPipeline::Builder::SetShader(ShaderType type, const GALShader& shader) {
+  switch (type) {
+  case ShaderType::Vertex:
+    vert_shader_ = shader;
+    break;
+  case ShaderType::Fragment:
+    frag_shader_ = shader;
+    break;
+  default:
+    throw Exception("Shader type not supported.");
+  }
+  return *this;
+}
+
+GALPipeline::Builder& GALPipeline::Builder::SetViewport(const Viewport& viewport) {
+  viewport_ = viewport;
+  return *this;
+}
+
+GALPipeline::Builder& GALPipeline::Builder::AddVertexInput(const VertexInput& vert_input) {
+  vert_inputs_.push_back(vert_input);
+  return *this;
+}
+
+GALPipeline::Builder& GALPipeline::Builder::AddVertexDesc(const VertexDesc& vert_desc) {
+  vert_descs_.push_back(vert_desc);
+  return *this;
+}
+
+GALPipeline::Builder& GALPipeline::Builder::AddUniformDesc(const UniformDesc& uniform_desc) {
+  uniform_descs_.push_back(uniform_desc);
+  return *this;
+}
+
+std::unique_ptr<GALPipeline> GALPipeline::Builder::Create() {
+  return std::make_unique<GALPipeline>(*this);
+}
 
 } // namespace gal
--- src/gal/gal_pipeline.h
+++ src/gal/gal_pipeline.h
@@ -1,14 +1,73 @@
 #ifndef GAL_GAL_PIPELINE_H_
 #define GAL_GAL_PIPELINE_H_
 
+#include <memory>
+#include <vector>
+#include "gal/gal_platform.h"
+#include "gal/gal_shader.h"
+
 namespace gal {
 
 class GALPipeline {
+
+// Forward declaration
+class Builder;
+
 public:
-  GALPipeline();
+  GALPipeline(Builder& builder);
   ~GALPipeline();
 
-private:
+public:
+  struct Viewport {
+    float x = 0.f;
+    float y = 0.f;
+    float width = 0.f;
+    float height = 0.f;
+  };
+
+  struct VertexInput {
+    int buffer_idx = 0;
+    int stride = 0;
+  };
+
+  struct VertexDesc {
+    int buffer_idx = 0;
+    int shader_idx = 0;
+    int num_components = 0;
+    int offset = 0;
+  };
+
+  struct UniformDesc {
+    int shader_idx = 0;
+    ShaderType shader_stage = ShaderType::Invalid;
+  };
+
+  class Builder {
+  friend class GALPipeline;
+
+  public:
+    Builder(GALPlatform* gal_platform) : gal_platform_(gal_platform) {}
+
+    Builder& SetShader(ShaderType type, const GALShader& shader);
+    Builder& SetViewport(const Viewport& viewport);
+    Builder& AddVertexInput(const VertexInput& vert_input);
+    Builder& AddVertexDesc(const VertexDesc& vert_desc);
+    Builder& AddUniformDesc(const UniformDesc& uniform_desc);
+    
+    std::unique_ptr<GALPipeline> Create();
+
+  private:
+    GALPlatform* gal_platform_;
+
+    GALShader vert_shader_;
+    GALShader frag_shader_;
+
+    Viewport viewport_;
+
+    std::vector<VertexInput> vert_inputs_;
+    std::vector<VertexDesc> vert_descs_;
+    std::vector<UniformDesc> uniform_descs_;
+  };
 };
 
 } // namespace gal
